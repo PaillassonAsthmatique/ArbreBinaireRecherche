ebe763d30759e7bb5f7fd1911027742d769b6e57
Cédric HAMEL <cedric.hamel@etu.univ-nantes.fr>
2018-11-08 11:57:44 +0100

    Ajout d'un Runnable et création des méthodes entrer() et sortir()

--- Toilettes/src/app/Toilettes.java
+++ Toilettes/src/app/Toilettes.java
@@ -5,8 +5,29 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Toilettes {
 
 	private AtomicInteger nbHomme = new AtomicInteger(0);
-	
 	private AtomicInteger nbFemme = new AtomicInteger(0);
+	
+	public void entrer(String sexe) {
+		switch(sexe) {
+		case "homme" :
+			incrementerNbHomme();
+			break;
+		case "femme" :
+			incrementerNbFemme();
+			break;
+		}	
+	}
+	
+	public void sortir(String sexe) {
+		switch(sexe) {
+		case "homme" :
+			decrementerNbHomme();
+			break;
+		case "femme" :
+			decrementerNbFemme();
+			break;
+		}	
+	}
 	
 	public void incrementerNbHomme() {
 		if ( nbHomme.get() < 3 && nbFemme.get() == 0) {
@@ -20,14 +41,14 @@ public class Toilettes {
 		}
 	}
 	
-	public void decrementernbHomme() {
-		if ( nbHomme.get() > 1 ) {
+	public void decrementerNbHomme() {
+		if ( nbHomme.get() >= 1 ) {
 			nbHomme.decrementAndGet();
 		}
 	}
 	
-	public void decrementernbFemme() {
-		if ( nbFemme.get() > 1 ) {
+	public void decrementerNbFemme() {
+		if ( nbFemme.get() >= 1 ) {
 			nbFemme.decrementAndGet();
 		}
 	}
