9b85ac4c3926834eedc794a4229d18f88d0aa321
Cédric HAMEL <cedric.hamel@etu.univ-nantes.fr>
2018-11-09 09:25:20 +0100

    Projet Toilettes OK, il manquera l'init de beaucoup de thread et éventuellement le rendre Starvfation Free

--- Toilettes/src/app/Main.java
+++ Toilettes/src/app/Main.java
@@ -6,10 +6,10 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Main {
 	public static void main(String[] args) {
 		
-		class MyRunnable implements Runnable
+		class Homme implements Runnable
 		{
 			private Toilettes wc;
-			MyRunnable (Toilettes wc)
+			Homme (Toilettes wc)
 			{
 			this.wc=wc;
 			}
@@ -21,12 +21,48 @@ public class Main {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+				System.out.println("Nombres d'hommes : " + wc.getNbHomme());
 				wc.sortir("homme");
+				System.out.println("Nombres d'hommes : " + wc.getNbHomme());
 			}
 		}
+		
+		class Femme implements Runnable
+		{
+			private Toilettes wc;
+			Femme (Toilettes wc)
+			{
+			this.wc=wc;
+			}
+			public void run()
+			{
+				wc.entrer("femme");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Nombres de femmes : " + wc.getNbFemme());
+				wc.sortir("femme");
+			}
+		}
+		
 		Toilettes wc = new Toilettes();
-		MyRunnable toiletteUnisexe = new MyRunnable(wc);
-		Thread tMain = new Thread(toiletteUnisexe);
-		tMain.start();
+		
+		Homme h1 = new Homme(wc);
+		Homme h2 = new Homme(wc);
+		Homme h3 = new Homme(wc);
+		
+		Thread th1 = new Thread(h1);
+		Thread th2 = new Thread(h2);
+		Thread th3 = new Thread(h3);
+		
+		Femme f1 = new Femme(wc);
+		
+		Thread tf1 = new Thread(f1);
+		th1.start();
+		tf1.start();
+		th2.start();
+		th3.start();
 	}	
 }
